2187ec9b4c6d38f31dac758cdafe437c5f2e02db
Fadel <fadel.g.heji@gmail.com>
2022-08-28 16:21:30 -0400

    done

--- my-app-frontend/src/Components/mealPlan.js
+++ my-app-frontend/src/Components/mealPlan.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 function mealPlan({ dayMeal}) {
-    const daysOfWeek = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thurday", "Friday"]
     return (
     <>
     <div className="container-fluid">
@@ -21,20 +20,6 @@ function mealPlan({ dayMeal}) {
 
 })}
 </div>
-
-    {/* <div className= "col-sm-2">
-        <h1> Sunday
-        {dayMeal.map( (meal, index)=> {
-        return <div className="card" style={{width: "18rem"}} key={index}>
-        <img className="card-img-top" src={meal.image} alt={meal.name}/>
-        <div className="card-body">
-        <h5 className="card-title" style = {{textAlign: "center"}}>{meal.name}	</h5>
-        <p className="card-text" style = {{textAlign: "center"}}>{meal.meal_type}</p>
-        </div>
-        </div>
-    })}
-    </h1>
-    </div> */}
 
 </div>
 </div>
